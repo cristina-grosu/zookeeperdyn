81de8414d74326ee398d50c76fc10757eb379160
cristina-grosu <cristina.grosu@bigstep.com>
2016-04-28 13:38:46 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,20 +5,29 @@ if [[ -z "${ZK_ID}" || -z "${ZK_SERVERS}" ]]; then
 fi
 echo "${ZK_SERVERS}" | tr ' ' '\n' | tee -a /etc/zookeeper/conf/zoo.cfg
 echo "${ZK_ID}" | tee /var/lib/zookeeper/myid
-/usr/share/zookeeper/bin/zkServer.sh start-foreground
-1
-2
-3
-4
-5
-6
-7
-8
-#!/bin/bash
-if [[ -z "${ZK_ID}" || -z "${ZK_SERVERS}" ]]; then
-       echo "Please set ZK_ID and ZK_SERVERS environment variables first."
-       exit 1
-fi
-echo "${ZK_SERVERS}" | tr ' ' '\n' | tee -a /etc/zookeeper/conf/zoo.cfg
-echo "${ZK_ID}" | tee /var/lib/zookeeper/myid
 /usr/share/zookeeper/bin/zkServer.sh start
+
+if [[ -z "$KAFKA_BROKER_ID" ]]; then
+  export KAFKA_BROKER_ID=$KAFKA_ADVERTISED_PORT
+fi
+if [[ -z "$KAFKA_LOG_DIRS" ]]; then
+  export KAFKA_LOG_DIRS="/kafka/kafka-logs-$KAFKA_BROKER_ID"
+fi
+if [[ -n "$KAFKA_HEAP_OPTS" ]]; then
+  sed -r -i "s/(export KAFKA_HEAP_OPTS)=\"(.*)\"/\1=\"$KAFKA_HEAP_OPTS\"/g" $KAFKA_HOME/bin/kafka-server-start.sh
+  unset KAFKA_HEAP_OPTS
+fi
+for VAR in `env`
+do
+if [[ $VAR =~ ^KAFKA_ && ! $VAR =~ ^KAFKA_HOME ]]; then
+  kafka_name=`echo "$VAR" | sed -r "s/KAFKA_(.*)=.*/\1/g" | tr '[:upper:]' '[:lower:]' | tr _ .`
+  env_var=`echo "$VAR" | sed -r "s/(.*)=.*/\1/g"`
+  if egrep -q "(^|^#)$kafka_name=" $KAFKA_HOME/config/server.properties; then
+    sed -r -i "s@(^|^#)($kafka_name)=(.*)@\2=${!env_var}@g" $KAFKA_HOME/config/server.properties #note that no config values may contain an '@' char
+  else
+    echo "$kafka_name=${!env_var}" >> $KAFKA_HOME/config/server.properties
+fi
+fi
+done
+
+$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
