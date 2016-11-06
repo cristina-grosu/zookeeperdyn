2486d78bda7fd0d1d35da356465bfbd69d0ce08a
cristina-grosu <cristina.grosu@bigstep.com>
2016-11-06 21:03:41 +0200

    Update zk-init.sh

--- zk-init.sh
+++ zk-init.sh
@@ -29,16 +29,21 @@ else
 		index=$(echo $line | grep -oE "Address [0-9]*:" | grep -oE "[0-9]*")
 	        index=$(($index + 0))
 		
-		if [ "$ip" == "$local_ip" ]; then
-	
-			echo "`bin/zkCli.sh -server $ZK:2181 get /zookeeper/config|grep ^server`" >> $ZK_HOME/conf/zoo.cfg.dynamic
-  			echo "server.$index=$ip:2888:3888:observer;2181" >> $ZK_HOME/conf/zoo.cfg.dynamic
-    			cp $ZK_HOME/conf/zoo.cfg.dynamic $ZK_HOME/conf/zoo.cfg.dynamic.org
+		if [[ "$ip" == "$local_ip" ] && [$index -eq 0 ]]; then
+			echo "server.$index=$local_ip:2888:3888;2181" >> $ZK_HOME/conf/zoo.cfg.dynamic
   			$ZK_HOME/bin/zkServer-initialize.sh --force --myid=$index
-  			ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' $ZK_HOME/bin/zkServer.sh start
-  			$ZK_HOME/bin/zkCli.sh -server $ZK:2181 reconfig -add "server.$index=$ip:2888:3888:participant;2181"
-  			$ZK_HOME/bin/zkServer.sh stop
   			ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' $ZK_HOME/bin/zkServer.sh start-foreground
+		else
+			if [ "$ip" == "$local_ip" ]; then
+				echo "`bin/zkCli.sh -server $ZK:2181 get /zookeeper/config|grep ^server`" >> $ZK_HOME/conf/zoo.cfg.dynamic
+  				echo "server.$index=$i:2888:3888:observer;2181" >> $ZK_HOME/conf/zoo.cfg.dynamic
+    				cp $ZK_HOME/conf/zoo.cfg.dynamic $ZK_HOME/conf/zoo.cfg.dynamic.org
+  				$ZK_HOME/bin/zkServer-initialize.sh --force --myid=$index
+  				ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' $ZK_HOME/bin/zkServer.sh start
+  				$ZK_HOME/bin/zkCli.sh -server $ZK:2181 reconfig -add "server.$index=$ip:2888:3888:participant;2181"
+  				$ZK_HOME/bin/zkServer.sh stop
+  				ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' $ZK_HOME/bin/zkServer.sh start-foreground
+			fi
 		fi
 	done < 'zk.cluster'
 fi
