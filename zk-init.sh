b7f86b456f61b3345776c72b4605a81eb26ea605
cristina-grosu <cristina.grosu@bigstep.com>
2016-11-06 22:03:35 +0200

    Update zk-init.sh

--- zk-init.sh
+++ zk-init.sh
@@ -2,7 +2,7 @@
 
 # Determine the local ip
 ifconfig | grep -oE "\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b" >> output
-local_ip=$(head -n1 output) && \
+local_ip=$(head -n1 output)
 rm output
 
 nslookup $HOSTNAME
@@ -23,7 +23,10 @@ while read line; do
 	index=$(echo $line | grep -oE "Address [0-9]*:" | grep -oE "[0-9]*")
         index=$(($index + 0))
 	
-	if [ "$ip" == "$local_ip" ] && [$index -eq 0 ]; then
+	if [ $index -eq 1 ]; then 
+		ZK=$ip
+	fi
+	if [ "$ip" == "$local_ip" ] && [ $index -eq 1 ]; then
 		echo "server.$index=$local_ip:2888:3888;2181" >> $ZK_HOME/conf/zoo.cfg.dynamic
   		$ZK_HOME/bin/zkServer-initialize.sh --force --myid=$index
   		ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' $ZK_HOME/bin/zkServer.sh start-foreground
