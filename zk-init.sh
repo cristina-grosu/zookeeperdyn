b376f903b29fe66a6a99427ca1fd24642ada3387
cristina-grosu <cristina.grosu@bigstep.com>
2016-11-06 22:33:33 +0200

    Update zk-init.sh

--- zk-init.sh
+++ zk-init.sh
@@ -5,6 +5,8 @@ ifconfig | grep -oE "\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.){3}(25[0-5]|2[0-4
 local_ip=$(head -n1 output)
 rm output
 
+sleep 30
+
 nslookup $HOSTNAME
 nslookup $HOSTNAME >> zk.cluster
 
